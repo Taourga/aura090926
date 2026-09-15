bb27c4f1f4cf60cfe65c7f844c96473f0dd7d059
Taourga <saibi.yanis@gmail.com>
2026-09-15 22:26:02 +0200

    style(doctor): charger la couche epuree

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ import "./simplification.css";
 import "./simplification-mobile-patch.css";
 import "./tassadite.css";
 import "./patient-inbox-header.css";
+import "./doctor-epure.css";
 
 export const metadata: Metadata = {
   title: "AURA Demo | Orchestration du séjour patient",
